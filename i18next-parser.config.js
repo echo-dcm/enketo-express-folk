0a4a93446532af73afe57709de23e821ab2c8898
ron-dcm <134161101+ron-dcm@users.noreply.github.com>
2023-06-15 00:58:41 +0630

    Add en as an option

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -56,7 +56,7 @@ module.exports = {
     lineEnding: 'auto',
     // Control the line ending. See options at https://github.com/ryanve/eol
 
-    locales: ['my'],
+    locales: ['my', 'en'],
     // An array of the locales in your applications
 
     namespaceSeparator: ':',

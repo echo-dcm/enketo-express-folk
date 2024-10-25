0753b659c564e3c825b8582b2adc72b5512ac738
echo-dcm <134759008+echo-dcm@users.noreply.github.com>
2024-10-25 10:53:23 +0630

    Update controller-webform.js

--- public/js/src/module/controller-webform.js
+++ public/js/src/module/controller-webform.js
@@ -454,6 +454,7 @@ function _submitRecord(survey) {
                 window?.ReactNativeWebView?.postMessage(JSON.stringify(hittinePostMessage));
                 // For web
                 window.parent.postMessage(JSON.stringify(hittinePostMessage));
+                console.log('hittinePostMessage is setup for HTWEB');
                 msg = msg.length > 0 ? msg : t('alert.submissionsuccess.msg');
                 gui.alert(msg, t('alert.submissionsuccess.heading'), level);
                 // _resetForm(survey);
